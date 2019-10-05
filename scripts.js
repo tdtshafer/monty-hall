0745931a6fb6e6a625ea148a9999720c61021625
Travis Shafer <tdtshafer@gmail.com>
2019-10-05 14:54:48 +0100

    Add collage and more logic

--- scripts.js
+++ scripts.js
@@ -1,13 +1,53 @@
+let stateTransition = {
+    "closed": "selected",
+    "selected": "selected",
+}
+
+var doors;
+var gameStage = 0;
+
 window.onload = function() {
+    doors = Array.from(document.getElementsByClassName('door'));
+    console.log(doors);
+    doors.forEach(function(door){
+        doorTransitionHandler(door)
+    })
+};  
 
-    document.getElementById('door_one').addEventListener('click', function (e) {
-      var img = document.createElement('img');
-      img.setAttribute('src', 'door_one_open.png');
-      e.target.appendChild(img);
-    });
-  
-  };
+function formatDoorString(string, doorNumber, doorState){
+    return string.replace('doorNumber', doorNumber)
+                 .replace('doorState', doorState);
+}
 
-function openDoor(x){
-    console.log(x)
+function doorTransitionHandler(door){
+    door.addEventListener('click', doorListener );
+}
+
+function doorListener(e){
+    let doorNumber = e.srcElement.id;
+    let doorState = e.srcElement.alt.split(' ').slice(-1)[0];
+    let newDoorState = stateTransition[doorState];
+    console.log(newDoorState);
+    console.log(stateTransition);
+    let newImageString = formatDoorString(
+        'images/door_doorNumber_doorState.png', 
+        doorNumber, 
+        newDoorState
+    );
+    let newImageAlt = formatDoorString(
+        'Door doorNumber: doorState', 
+        doorNumber, 
+        newDoorState
+    );
+    console.log(doorNumber);
+    console.log(doorState);
+    e.srcElement.setAttribute('alt', newImageAlt);
+    e.srcElement.setAttribute('src', newImageString);
+    doors.forEach(function(door){
+        door.removeEventListener('click', doorListener)
+    })
+    let commands = document.getElementById('text_commands');
+    console.log(commands);
+    commands.innerHTML = "";
+
 }
